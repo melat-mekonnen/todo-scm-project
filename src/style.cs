5145d5fd5e0c8117068478fb33b762374346f158
melat-mekonnen <127474013+melat-mekonnen@users.noreply.github.com>
2025-12-21 12:47:06 +0300

    Initial version 0.1: login, dashboard, add task, empty JSON

---
+++ src/style.cs
@@ -0,0 +1,17 @@
+body {
+  font-family: Arial, sans-serif;
+  margin: 30px;
+}
+
+input {
+  padding: 5px;
+}
+
+button {
+  padding: 5px 10px;
+  margin-left: 5px;
+}
+
+ul {
+  margin-top: 20px;
+}
